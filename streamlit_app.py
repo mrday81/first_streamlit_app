be3015ff384c025c0deb2a547bbb49db85e71a30
mrday81 <83085810+mrday81@users.noreply.github.com>
2022-08-28 13:00:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 # display fruity
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
