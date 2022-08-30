1b4f6bdb1223a01ef5a3795d248c622a8b37eaae
mrday81 <83085810+mrday81@users.noreply.github.com>
2022-08-30 19:03:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,10 @@ streamlit.dataframe(my_data_row)
 streamlit.header("What fruit would you like to add:")
 streamlit.text("Jackfruit")
 
+streamlit.header("What fruit would you like to add")
+# display response
+add_my_fruit = streamlit.text_input('What fruit would you like to add:')
+streamlit.write('The user entered ', add_my_fruit)
+
+
+
